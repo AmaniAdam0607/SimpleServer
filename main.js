e4d109269a2c476788ac4cfcd2841062bed5f6c9
AmaniAdam0607 <amaniadam178@gmail.com>
2024-05-12 12:04:41 +0300

    add route to list all todos in json

--- main.js
+++ main.js
@@ -2,25 +2,27 @@ const express = require('express');
 
 const app  = express();
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.post("/", ( req, res ) => {
-    spitInfo(req);
-    res.send("<h1>Aloooh!!</h1>");
+const todos = [
+    {
+        "title": "Study Express",
+        "Description": "Study how to make simple working web servers."
+    }
+]
+
+
+app.get("/todos", ( req, res ) => {
+    res.send(todos);
+    res.end()
 });
 
-app.get("/news/:about_time", ( req, res ) => {
-    spitInfo(req);
-    res.send(`Reuest for ${req.url}`);
-});
-
-const spitInfo = ( req ) => {
-    console.log(`Request for ${req.url}`);
-    console.log(`Request body ${req.body}`);
-    console.log(`Request query ${req.query}`);
-    //console.log(`With params ${JSON.stringify(req.params)}`);
-}
+app.get("todo/:id", (req, res) => {
+    console.log(req.url)
+    const { id } = req.params;
+    res.send(todos[id]);
+    res.end();
+})
 
 app.listen(3000, () => {
     console.log(`Server is listening for requests!!`)
