9268f6760cf50578335c6b2a5da15866127808ba
AmaniAdam0607 <amaniadam178@gmail.com>
2024-05-13 11:44:49 +0300

    add product api

---
+++ data.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Dell Laptop",
+        "Description": "A dell laptop for the ages",
+        "Specifications": {
+            "RAM": 8,
+            "Processor": 2.3,
+            "Color": "blue"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Mac Laptop",
+        "Description": "A dell laptop for the ages",
+        "Specifications": {
+            "RAM": 8,
+            "Processor": 3.3,
+            "Color": "blue"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Laptop",
+        "Description": "A dell laptop for the ages",
+        "Specifications": {
+            "RAM": 16,
+            "Processor": 5.3,
+            "Color": "blue"
+        }
+    }
+]
+
+module.exports  = { products }
--- main.js
+++ main.js
@@ -1,27 +1,46 @@
+/**
+ * A simple server to provide an API for an ecommerce site
+ * The API serves product listings and simple routes for the listing manipulation
+ * Written by Amani Adam
+ * 
+ * "Into it"
+ */
+
+
+const { products } = require("./data")
 const express = require('express');
 
 const app  = express();
 
-app.use(express.json());
-
-const todos = [
-    {
-        "title": "Study Express",
-        "Description": "Study how to make simple working web servers."
-    }
-]
+//app.use(express.json()); //this is a middleware. What is a middleware??
 
 
-app.get("/todos", ( req, res ) => {
-    res.send(todos);
-    res.end()
+app.get("/api/v1/products", ( req, res ) => {
+    res.json(products) // this sends a json response, set the content header for us
 });
 
-app.get("todo/:id", (req, res) => {
-    console.log(req.url)
+app.get("/api/v1/products/:id", (req, res) => {
+    //console.log(req.params)
     const { id } = req.params;
-    res.send(todos[id]);
-    res.end();
+    //console.log(id)
+    const product = products.find((product) => product.id === Number(id) ) // find takes in a predicate
+    if ( !product ) { // the find method returned undefined
+        return res.json({"status": "FAIL", "message": "either param type or product id not found"})
+    }
+    return res.json(product)
+    //res.json(todos[id]); // not a good idea because the params are all strings
+})
+
+app.get("/api/v1/products/search/name", ( req, res ) => {
+    const { q } = req.query // extracts the search query
+    let searchableProducts = [...products]
+    const productMatchingQuery = []
+    searchableProducts.forEach( (product) => {
+        if (product.name.toLowerCase().includes(q.toLowerCase())){
+            return productMatchingQuery.push(product)
+        }
+    })
+    res.json(productMatchingQuery)
 })
 
 app.listen(3000, () => {
